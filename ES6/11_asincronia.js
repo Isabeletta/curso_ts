366201ea69feae0e2fc1ed82797d8bd02a7369bc
Isabeletta <isabeletta@hotmail.com>
2019-07-10 08:35:08 +0200

    Dia 2

---
+++ ES6/11_asincronia.js
@@ -0,0 +1,15 @@
+// JA lenguaje monohilo
+
+// Tareas que consumen tiempo, pueden ser:
+//      - Tareas bloqueantes
+//      - Tareas no bloqueantes o asíncronas
+
+// Bucle de eventos
+
+setTimeout( () => console.log('Linea 1'), 0)
+console.log('Linea 2')
+console.log('Linea 3')
+setTimeout( () => console.log('Linea 4'), 2000)
+console.log('Linea 5')
+setTimeout( () => console.log('Linea 6'), 1000)
+console.log('Linea 7')
---
+++ ES6/12_asincronia.js
@@ -0,0 +1,52 @@
+
+// Mecanismos para resolver la sincronica
+
+// Callbacks
+
+function leerDatos(callback) {
+    setTimeout( () => {
+        let data = 'los datos Callbacks'
+        // return data
+        callback(data)
+    }, 1000)
+}
+
+function mostrarDatos(datos) {
+    console.log(datos)
+}
+
+// mostrarDatos(leerDatos()) Esto devuelve undefined
+
+leerDatos(mostrarDatos)
+
+// ES6 Promesas
+
+function leerDatosPromesa() {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            const n = Math.random() 
+            if(n < 0.75) {
+                // OK
+                resolve(n)
+            } else {
+                // Error
+                reject(new Error(n))
+            }
+        }, 2000)
+
+    }) 
+}
+
+leerDatosPromesa()
+    .then( (datos ) => { console.log("Promesas todo ok " + datos)})
+    .catch( ( error ) => { console.log("Error Promesas: " + error.message)});
+
+
+// ES2017 (ES8) async/await
+
+( function ()  {
+    leerDatosPromesa()
+        .then( (datos ) => { console.log("Promesas todo ok " + datos)})
+        .catch( ( error ) => { console.log("Error Promesas: " + error.message)})
+})()
+
